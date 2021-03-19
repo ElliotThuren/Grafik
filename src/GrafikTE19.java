859e6014662bbcc64b30976a1d9181de3f8c2a12
ElliotThuren <elliot.thuren@elev.ga.ntig.se>
2021-03-19 11:19:45 +0100

    19/3-2021

--- src/GrafikTE19.java
+++ src/GrafikTE19.java
@@ -1,14 +1,63 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.BufferStrategy;
+import java.awt.image.BufferedImage;
 
-public class GrafikTE19 extends Canvas {
+public class GrafikTE19 extends Canvas implements Runnable {
+    private int width = 800;
+    private int height = 600;
+
+    private Thread thread;
+    int fps = 30;
+    private boolean isRunning;
+
+    private BufferStrategy bs;
+
+    private int houseX, houseY;
+    private int houseVX, houseVY;
+
+    private int treeX, treeY;
+
     public GrafikTE19() {
         JFrame frame = new JFrame("A simple painting");
         this.setSize(800,600);
         frame.add(this);
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.addKeyListener(new KL());
         frame.setVisible(true);
+
+        isRunning = false;
+
+        houseX = 300;
+        houseY = 150;
+        houseVX = 1;
+        houseVY = 0;
+
+        treeX = 200;
+        treeY = 200;
+    }
+
+    public void update() {
+        houseX += houseVX;
+        if (houseX > width){
+            houseVX = -1;
+        }
+        if (houseX < 0 ) {
+            houseVX = 1;
+        }
+    }
+
+    public void draw() {
+        bs = getBufferStrategy();
+        if (bs == null) {
+            createBufferStrategy(3);
+            return;
+        }
+        Graphics g = bs.getDrawGraphics();
+
+        update();
     }
 
     public void paint(Graphics g) {
